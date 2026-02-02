2ef07f59dacc15535c7888f8c3caf2123b087bfc
vaibhavkhugshal <vaibhavkhugshal010@gmail.com>
2026-02-02 22:41:29 +0530

    Added comments to improve readability

--- Java/Practice/sortedArray.java
+++ Java/Practice/sortedArray.java
@@ -1,5 +1,9 @@
+//Problem-- Check if the array is sorted.
+
 public class sortedArray {
-    public static boolean ifArrayIsSorted(int arr[]) {
+    public static boolean ifArrayIsSorted(int arr[]) { //function to check if array is sorted
+        
+        //Traversing the array
         for (int i = 0; i < arr.length-1; i++) {
             if (arr[i] > arr[i+1]) {
                 return false;
@@ -11,6 +15,7 @@ public class sortedArray {
     public static void main(String[] args) {
         int arr[] = { 1, 2, 5, 6, 7, 8 };
 
+        //Calling function and printing results
         System.out.println(ifArrayIsSorted(arr));
     }
 }
