7dde24c65d3adf2fa3bf2d1b364d7d046f674054
vaibhavkhugshal <vaibhavkhugshal010@gmail.com>
2026-02-16 23:50:30 +0530

    Initial Commit

---
+++ Java/Practice/smallestLetter.java
@@ -0,0 +1,24 @@
+//Problem Statement-- 
+
+public class smallestLetter {
+    public static char nextGreatestLetter(char[] letters, char target) {
+        int start = 0;
+        int end = letters.length - 1;
+        while (start <= end) {
+            int mid = (start + (end - start) / 2);
+            if (target < letters[mid]) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+
+        }
+        return letters[start % letters.length];
+    }
+
+    public static void main(String[] args) {
+        char[] letters = { 'c', 'f', 'j' };
+        char target = 'c';
+        System.out.println(nextGreatestLetter(letters, target));
+    }
+}
